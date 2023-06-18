ef46d2bf8595a0d5288d1e61968f4df3df61442f
Mihajarisoa <edge7raza@gmail.com>
2023-06-18 16:03:56 +0300

    Ajout de styles et animations JS à la page HOME

---
+++ scripts/scroll-zoom2.js
@@ -0,0 +1,74 @@
+document.addEventListener("DOMContentLoaded", function() {
+  if (window.innerWidth <= 1024) {
+    const character = document.querySelector('.inline-images2').querySelectorAll('.characters2');
+    let currentCharacterIndex = 0;
+  
+    function showCharacter(index) {
+      character.forEach((character, characterIndex) => {
+        if (characterIndex === index) {
+          character.classList.add('active');
+        } else {
+          character.classList.remove('active');
+        }
+      });
+  
+    updateNavigation(index);
+    }
+  
+    function updateNavigation(index) {
+      const navItems = document.querySelectorAll('.nav-items2');
+      navItems.forEach((item, itemIndex) => {
+        if (itemIndex === index) {
+          item.classList.add('active');
+        } else {
+          item.classList.remove('active');
+        }
+      });
+    }
+  
+    function nextCharacter() {
+      currentCharacterIndex = (currentCharacterIndex + 1) % character.length;
+      showCharacter(currentCharacterIndex);
+    }
+  
+    setInterval(nextCharacter, 3000);
+
+    character.forEach(character => {
+      character.addEventListener('mouseenter', function() {
+        this.style.transform = 'scale(1.1)';
+        const actor = this.querySelector('.actor');
+        if (actor) {
+          actor.style.display = 'flex';
+        }
+      });
+      character.addEventListener('mouseleave', function() {
+        this.style.transform = 'scale(1)';
+        const actor = this.querySelector('.actor');
+        if (actor) {
+          actor.style.display = 'none';
+        }
+      })
+    });
+  }
+
+  else if (window.innerWidth > 1024) {
+    const character = document.querySelector('.inline-images2').querySelectorAll('.characters2');
+    character.forEach(character => {
+      character.addEventListener('mouseenter', function() {
+        this.style.transform = 'scale(0.9)';
+        const actor = this.querySelector('.actor');
+        if (actor) {
+          actor.style.display = 'flex';
+        }
+      });
+      character.addEventListener('mouseleave', function() {
+        this.style.transform = 'scale(0.8)';
+        const actor = this.querySelector('.actor');
+        if (actor) {
+          actor.style.display = 'none';
+        }
+      })
+    })
+  }
+});
+  
